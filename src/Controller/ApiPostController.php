62d95fe0ebef50d5edfc4848aa4259e0d51dd375
unknown <boivinsamuel.98@gmail.com>
2021-05-26 13:57:01 +0200

    26/05/2021

--- src/Controller/ApiPostController.php
+++ src/Controller/ApiPostController.php
@@ -2,27 +2,54 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Repository\PostRepository;
-use App\Repository\UserRepository;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+ use App\Entity\User;
+ use App\Repository\PostRepository;
+ use App\Repository\UserRepository;
+ use Symfony\Component\HttpFoundation\Response;
+ use Symfony\Component\Routing\Annotation\Route;
+ use Symfony\Component\HttpFoundation\JsonResponse;
+ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ApiPostController extends AbstractController
 {
-     public $serializer;
-
     /**
-     * Permet de récupérer la liste des users.
-     * @Route("/api/user/list", name="api_user_list", methods={"GET", "POST"})
+     * @Route("/api/user/list", name="api_user_list")
      */
-    
-    public function apiGetUser(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository): Response
     {
-        $userRepository = new Response();
-        return $this->json($userRepository);
+        $data = $userRepository->findAll();
         $serializer = \JMS\Serializer\SerializerBuilder::create()->build();
+        $serializer->serialize($data, 'json');
+
+        return JsonResponse::fromJsonString($serializer->serialize($data, 'json'));
     }
 }
+
+// Mon code si dessous
+
+// namespace App\Controller;
+
+// use App\Entity\User;
+// use App\Repository\PostRepository;
+// use App\Repository\UserRepository;
+// use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\Routing\Annotation\Route;
+// use Symfony\Component\HttpFoundation\JsonResponse;
+// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+// class ApiPostController extends AbstractController
+// {
+//      public $serializer;
+
+//     /**
+//      * Permet de récupérer la liste des users.
+//      * @Route("/api/user/list", name="api_user_list", methods={"GET", "POST"})
+//      */
+
+//     public function apiGetUser(UserRepository $userRepository): Response
+//     {
+//         $userRepository = new Response();
+//         return $this->json($userRepository);
+//         $serializer = \JMS\Serializer\SerializerBuilder::create()->build();
+//     }
+// }
--- src/Controller/FicheFraisController.php
+++ src/Controller/FicheFraisController.php
@@ -2,12 +2,20 @@
 
 namespace App\Controller;
 
+use DateTime;
 use Exception;
+use DateTimeZone;
+use App\Entity\EtatFiche;
+use App\Entity\FicheFrais;
+use App\Entity\StatutLigne;
 use App\Entity\FraisForfait;
+use App\Entity\Justificatif;
+use App\Entity\LigneFraisForfait;
+use App\Entity\LigneFraisHorsForfait;
 use App\Repository\FicheFraisRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
@@ -33,41 +41,9 @@ class FicheFraisController extends AbstractController
      * @param EntityManagerInterface $manager
      * @return Response
      */
-    public function FicheFraisnew(EntityManagerInterface $manager): Response
+    public function newFicheFrais(EntityManagerInterface $manager): Response
     {
         $fraisForfaits = $manager->getRepository(FraisForfait::class)->findAll();
-//         $newFileName = tempnam(sys_get_temp_dir(), 'myAppNamespace');
-
-        return $this->render('fiche_frais/new.html.twig', [
-            'fraisForfaits' => $fraisForfaits
-        ]);
-    }
-
-/**
-     * @Route("/fiche/frais/hors/forfait", name="fiche_frais_hors_forfait")
-     * @param EntityManagerInterface $manager
-     * @return Response
-     */
-    public function FicheFraisHorsForfait(EntityManagerInterface $manager): Response
-    {
-        $fraisForfaits = $manager->getRepository(FraisForfait::class)->findAll();
-//         $newFileName = tempnam(sys_get_temp_dir(), 'myAppNamespace');
-
-        return $this->render('fiche_frais/new.html.twig', [
-            'fraisForfaits' => $fraisForfaits
-        ]);
-    }
-
-/**
-     * @Route("/fiche/frais/hors/forfait/new", name="fiche_frais_hors_forfait_new")
-     * @param EntityManagerInterface $manager
-     * @return Response
-     */
-    public function FicheFraisHorsForfaitNew(EntityManagerInterface $manager): Response
-    {
-        $fraisForfaits = $manager->getRepository(FraisForfait::class)->findAll();
-//         $newFileName = tempnam(sys_get_temp_dir(), 'myAppNamespace');
-
         return $this->render('fiche_frais/new.html.twig', [
             'fraisForfaits' => $fraisForfaits
         ]);
@@ -82,24 +58,61 @@ class FicheFraisController extends AbstractController
      */
     public function storeFicheFrais(EntityManagerInterface $manager, Request $request): Response
     {
-        foreach ($request->files->get('files') as $file){
+        $user = $this->getUser();
+        $dateNow = new DateTime(null, new DateTimeZone('Europe/Paris'));
+        $dateEemission = DateTime::createFromFormat('Y-m-d', $request->get('dateFicheFrais'));
+        $ficheFrais = new FicheFrais();
+        $ficheFrais->setDateCreationFicheFrais($dateNow);
+        $ficheFrais->setDateFicheFrais($dateEemission);
+        $ficheFrais->setDateModificationFicheFrais($dateNow);
+        $ficheFrais->setEtatFicheFrais($manager->getRepository(EtatFiche::class)->find(1));
+        $ficheFrais->setNbJustificatif(1);
+        $ficheFrais->setUtilisateurFicheFrais($user);
+        $manager->persist($ficheFrais);
+        $manager->flush();
+        $dateJustificatifs = $request->request->get('dateJustificatifs');
+        $quantites = $request->request->get('quantites');
+        $montants = $request->request->get('montants');
+        $forfaits = $request->request->get('forfaits');
+
+        foreach ($request->files->get('files') as $key => $file){
 
             $filesystem = new Filesystem();
 
             try {
-                $filesystem->mkdir(sys_get_temp_dir().'/'.random_int(0, 1000));
+                $filesystem->mkdir('uploads/tmp/');
+                $filesystem->mkdir('uploads/justificatif/');
             } catch (IOExceptionInterface $exception) {
                 echo "An error occurred while creating your directory at ".$exception->getPath();
             }
 
-            dd($request);
-
+            $tempFile = $filesystem->tempnam($this->getParameter('uploads_directory') . '/tmp', 'justif_');
+            $finalFile = $filesystem->tempnam($this->getParameter('uploads_directory') . '/justificatif', 'justif_');
+            $filesystem->remove($tempFile);
             $orinalNameFile = $file->getClientOriginalName();
-
+            $justifacatif = new Justificatif();
+            $justifacatif->setChemin($finalFile);
+            $justifacatif->setDateCreationJustificatif($dateNow);
+            $justifacatif->setDateProductionJustificatif(DateTime::createFromFormat('Y-m-d', $dateJustificatifs[$key]));
+            $justifacatif->setMontant($montants[$key]);
+            $justifacatif->setUtilisateurJustificatif($user);
+            $manager->persist($justifacatif);
+            $manager->flush();
+            $ligneFraisForfait = new LigneFraisForfait();
+            $ligneFraisForfait->setDateCreationLigneFraisForfait($dateNow);
+            $ligneFraisForfait->setDateLigneFraisForfait($dateEemission);
+            $ligneFraisForfait->setFicheFrais($ficheFrais);
+            $ligneFraisForfait->setFraisForfait($manager->getRepository(FraisForfait::class)->findOneBy(['libelle' => $forfaits[$key]]));
+            $ligneFraisForfait->setDateCreationLigneFraisForfait($dateNow);
+            $ligneFraisForfait->setQuantite($quantites[$key]);
+            $ligneFraisForfait->setUtilisateurLigneFraisForfait($user);
+            $ligneFraisForfait->setStatutLigneFraisForfait($manager->getRepository(StatutLigne::class)->find(1));
+            $manager->persist($ligneFraisForfait);
+            $manager->flush();
+            $ficheFrais->setNbJustificatif($key + 1);
+            $manager->flush();
         }
 
-        dd($request->files);
-
-        return $this->render('fiche_frais/new.html.twig', []);
+        return $this->redirectToRoute('fiche_frais_new');
     }
 }
